e800fd24861ea73b63096b112004ac69b3b7b949
Tina Kamali-Anaraki <tkamali-anaraki@myseneca.ca>
2020-10-13 22:05:17 -0400

    style: prettier formatting

--- src/service.ts
+++ src/service.ts
@@ -2,10 +2,10 @@ import express from "express";
 const app = express();
 const port = 8080;
 
-app.get('*', (req,res)=>{
+app.get("*", (req, res) => {
   res.status(500).send("Unimplemented Feature");
 });
 
-app.listen( port, ()=>{
-  console.log( `server started. Listening at ${ port }` );
+app.listen(port, () => {
+  console.log(`server started. Listening at ${port}`);
 });
