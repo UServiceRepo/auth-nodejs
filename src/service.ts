48559d9e8eb2f5655b8fd98fd7026a6b20ed6086
rdittrich97 <rdittrich@myseneca.ca>
2020-10-15 18:05:06 -0400

    initial work

--- src/service.ts
+++ src/service.ts
@@ -2,6 +2,36 @@ import express from "express";
 const app = express();
 const port = 8080;
 
+app.use(express.urlencoded());
+app.use(express.json());
+
+class auth {
+  id: string;
+  pass: string;
+  token: string;
+  constructor(json: {id: string, pass: string, token: string}) {
+    if (typeof(json.id) == "string")
+      this.id = json.id;
+    else
+      throw new Error("seg fault core dumped");
+    if (typeof(json.pass) == "string")
+      this.pass = json.pass;
+    else
+      throw new Error("seg fault core dumped");
+    if (typeof(json.token) == "string")
+      this.token = json.token;
+    else
+      throw new Error("seg fault core dumped");
+      
+  }
+};
+
+app.get("/authorization", (req, res) => {
+  const body = new auth(req.body);
+  console.log(body);
+  res.send("moicrosorev!!")
+});
+
 app.get("*", (req, res) => {
   res.status(500).send("Unimplemented Feature");
 });
