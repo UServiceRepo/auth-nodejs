fb97f67586fe887e60f52a4b109c0d75c4bcebd5
rdittrich97 <rdittrich@myseneca.ca>
2020-11-12 17:48:50 -0500

    cleaned up error handling

--- src/models/AuthReq.ts
+++ src/models/AuthReq.ts
@@ -1,27 +1,16 @@
 export default class AuthReq {
-    id: string;
-    pass: string;
-    token: string;
-    valid: boolean;
+    id: string = "";
+    pass: string = "";
+    token: string = "";
+    valid: boolean = false;
     constructor(json: {id: string, pass: string, token: string, valid: boolean}) {
-      if (typeof(json.id) == "string")
-        this.id = json.id;
-      else
-        throw new TypeError(`"id" is not of type ${typeof(this.id)}`);
-      
-      if (typeof(json.pass) == "string")
-        this.pass = json.pass;
-      else
-        throw new TypeError(`"pass" is not of type ${typeof(this.pass)}`);
-      
-      if (typeof(json.token) == "string")
-        this.token = json.token;
-      else
-        throw new TypeError(`"token" is not of type ${typeof(this.token)}`); 
-      
-      if (typeof(json.valid) == "boolean")
-        this.valid = json.valid;
-      else 
-        throw new TypeError(`"valid" is not of type ${typeof(this.valid)}`); 
+
+      Object.keys(json).forEach((key: string) => {
+        if(typeof((this as any)[key]) == typeof((json as any)[key])) {
+          (this as any)[key] = (json as any)[key];
+        } else {
+          throw new TypeError(`"${key}" is not of type ${typeof((this as any)[key])}`);
+        }
+      });
     }
 };
